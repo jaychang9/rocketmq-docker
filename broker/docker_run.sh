5b97904a004580cc9f3c527d235871dd7d02b20b
zhangjie <zhangjie@choosefine.com>
2017-12-08 11:23:26 +0800

    修改映射

--- broker/docker_run.sh
+++ broker/docker_run.sh
@@ -2,4 +2,6 @@
 docker run -d -p 10911:10911 -p 10909:10909 --name rmqbroker --restart=always -m 2048m \
  -e "NAMESRV_ADDR=rmqnamesrv:9876" \
  -e "JAVA_OPT=-XX:MetaspaceSize=256m" \
+ -v /opt/rocketmq/logs:/opt/rocketmq/logs \
+ -v /opt/rocketmq/store:/opt/rocketmq/store \
   --link rmqnamesrv:rmqnamesrv apache/rocketmq-broker:4.1.0-incubating
